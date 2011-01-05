d24f2725b54c4813b10ccbd04f0ba49a74c4da96
ejc <ej@ejc.me>
2011-01-04 21:01:10 -0800

    HAML NOM NOM NOM; CSS Basics™; Fix Facebook URL bug; Add .DS_Store to .gitignore;

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -2,21 +2,21 @@ module AccountsHelper
   def link_to_service name, service
     case service.name
     when 'twitter'
-      link_to name, 'http://twitter.com/' + service.key
+      link_to name, 'http://twitter.com/' + service.key, :class => 'twitter'
     when 'friendfeed'
-      link_to name, 'http://friendfeed.com/' + service.key
+      link_to name, 'http://friendfeed.com/' + service.key, :class => 'friendfeed'
     when 'mixi'
-      link_to name, 'http://mixi.jp/show_friend.pl?id=' + service.key
+      link_to name, 'http://mixi.jp/show_friend.pl?id=' + service.key, :class => 'mixi'
     when 'github'
-      link_to name, 'http://github.com/' + service.key
+      link_to name, 'http://github.com/' + service.key, :class => 'github'
     when 'facebook'
-      if service.key =~ /^\sd+$/
-        link_to name, 'http://www.facebook.com/profile.php?id=' + service.key
+      if service.key =~ /^\s*\d+$/
+        link_to name, 'http://www.facebook.com/profile.php?id=' + service.key, :class => 'facebook'
       else
-        link_to name, 'http://www.facebook.com/' + service.key
+        link_to name, 'http://www.facebook.com/' + service.key, :class => 'facebook'
       end
     when 'iddy'
-      link_to name, 'http://iddy.jp/profile/' + service.key
+      link_to name, 'http://iddy.jp/profile/' + service.key, :class => 'iddy'
     end
   end
 end
