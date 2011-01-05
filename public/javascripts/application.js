3c512f8572d22a8e83a39dd3e1c08c02f0d92a38
Joe Sak <joe@joesak.com>
2011-01-04 20:26:41 -0600

    JS started, books started

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	$('.account').equalHeights();
+	//equalize the height of each account in the INDEX list
+	// #accounts is the scope that indicates the INDEX view
+	$('#accounts .account').equalHeights();
+	
+	
+	//gather the user names of each account in the INDEX list
+	var usernames = new Array();
+	$('#accounts .account').each(function(i){
+	  usernames[i] = $(this).find('h2').text();
+	})
+	
+	//bind the filter input on the INDEX list
+	$('#filter_by_username').change(function(){
+
+	})
+	
 });
