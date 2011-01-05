f0625a3f1923e6d4591254dd60e68a414a26901c
Joe Sak <joe@joesak.com>
2011-01-05 00:22:26 -0600

    link_to_service doesn't pass options? for shame! also, linking up services for iconization

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,22 +1,22 @@
 module AccountsHelper
-  def link_to_service name, service
+  def link_to_service name, service, options = {}
     case service.name
     when 'twitter'
-      link_to name, 'http://twitter.com/' + service.key
+      link_to name, 'http://twitter.com/' + service.key, options
     when 'friendfeed'
-      link_to name, 'http://friendfeed.com/' + service.key
+      link_to name, 'http://friendfeed.com/' + service.key, options
     when 'mixi'
-      link_to name, 'http://mixi.jp/show_friend.pl?id=' + service.key
+      link_to name, 'http://mixi.jp/show_friend.pl?id=' + service.key, options
     when 'github'
-      link_to name, 'http://github.com/' + service.key
+      link_to name, 'http://github.com/' + service.key, options
     when 'facebook'
       if service.key =~ /^\sd+$/
-        link_to name, 'http://www.facebook.com/profile.php?id=' + service.key
+        link_to name, 'http://www.facebook.com/profile.php?id=' + service.key, options
       else
-        link_to name, 'http://www.facebook.com/' + service.key
+        link_to name, 'http://www.facebook.com/' + service.key, options
       end
     when 'iddy'
-      link_to name, 'http://iddy.jp/profile/' + service.key
+      link_to name, 'http://iddy.jp/profile/' + service.key, options
     end
   end
 end
