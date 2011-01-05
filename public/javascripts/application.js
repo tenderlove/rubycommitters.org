4cc50c3cd06b71f61b732cc193e5f37fb8d95358
ejc <ej@ejc.me>
2011-01-05 15:13:31 -0800

    added texture, fork ribbon, user cards and scrollable

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function() {
+
+	// initialize scrollable
+  $(".scrollable").scrollable({ vertical: true, mousewheel: true });
+
+});
