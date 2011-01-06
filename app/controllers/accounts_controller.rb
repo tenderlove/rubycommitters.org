8d5ae7777648b2302516049c8f57a2c479fbbbd8
Joe Sak <joe@joesak.com>
2011-01-06 09:30:02 -0600

    Not using js responses

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,9 +5,5 @@ class AccountsController < ApplicationController
   
   def show
     @account = Account.find(params[:id])
-    respond_to do |format|
-      format.js { render :layout => false }
-      format.html
-    end
   end
 end
