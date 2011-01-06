5aaf3f19ed751f7f7abfa1ce7dec3d735fb5a81e
Fred Wu <ifredwu@gmail.com>
2011-01-07 10:29:03 +1100

    Use cache_control helper instead.

--- app/controllers/heroes.rb
+++ app/controllers/heroes.rb
@@ -2,7 +2,8 @@ RubyCommitters.controllers :heroes do
   get :index, :map => '/' do
     @heroes = Hero.all
     @attrs = %w{names nicks sites services books portraits}
-    response['Cache-Control'] = 'public, max-age=300'
+    
+    cache_control :public, :max_age => 300
     render 'heroes/index'
   end
 end
