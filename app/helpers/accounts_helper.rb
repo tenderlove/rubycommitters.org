3f0bbf5699cddb5ba28af523cc7e695901d07fb5
Matthew Anderson <manderson@onehub.com>
2011-01-07 01:05:18 -0800

    Everything but Iddy.

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -2,21 +2,21 @@ module AccountsHelper
   def link_to_service name, service
     case service.name
     when 'twitter'
-      link_to name, 'http://twitter.com/' + service.key
+      link_to name, 'http://twitter.com/' + service.key, :title => 'Twitter'
     when 'friendfeed'
-      link_to name, 'http://friendfeed.com/' + service.key
+      link_to name, 'http://friendfeed.com/' + service.key, :title => 'FriendFeed'
     when 'mixi'
-      link_to name, 'http://mixi.jp/show_friend.pl?id=' + service.key
+      link_to name, 'http://mixi.jp/show_friend.pl?id=' + service.key, :title => 'Mixi'
     when 'github'
-      link_to name, 'http://github.com/' + service.key
+      link_to name, 'http://github.com/' + service.key, :title => 'Github'
     when 'facebook'
       if service.key =~ /^\sd+$/
-        link_to name, 'http://www.facebook.com/profile.php?id=' + service.key
+        link_to name, 'http://www.facebook.com/profile.php?id=' + service.key, :title => 'Facebook'
       else
-        link_to name, 'http://www.facebook.com/' + service.key
+        link_to name, 'http://www.facebook.com/' + service.key, :title => 'Facebook'
       end
     when 'iddy'
-      link_to name, 'http://iddy.jp/profile/' + service.key
+      link_to name, 'http://iddy.jp/profile/' + service.key, :title => 'Iddy'
     end
   end
 end
