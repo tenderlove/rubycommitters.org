da9a72e20c12b5859699fbd824b804bd382dde4b
Nic Haynes <nic@nicinabox.com>
2011-01-08 17:40:49 -0600

    Fixed comma in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rubycommitters::Application.routes.draw do
   
   resources :accounts do 
-    get 'filter/:query' => 'accounts#filter', :on => :collection, 
+    get 'filter/:query' => 'accounts#filter', :on => :collection
   end
   root :to => 'accounts#index'
 end
