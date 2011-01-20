1d95f4d717cf1f74963ac7964cdb2d44f01397f8
Jared Mehle <jrmehle@gmail.com>
2011-01-19 22:36:06 -0600

    Polished the style.

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -2,21 +2,21 @@ module AccountsHelper
   def link_to_service name, service
     case service.name
     when 'twitter'
-      link_to image_tag("Twitter.png"), 'http://twitter.com/' + service.key
+      link_to image_tag("Twitter.png", :alt => 'Twitter'), 'http://twitter.com/' + service.key
     when 'friendfeed'
-      link_to image_tag("Friendfeed.png"), 'http://friendfeed.com/' + service.key
+      link_to image_tag("Friendfeed.png", :alt => 'FriendFeed'), 'http://friendfeed.com/' + service.key
     when 'mixi'
-      link_to image_tag("Mixi.png"), 'http://mixi.jp/show_friend.pl?id=' + service.key
+      link_to image_tag("Mixi.png", :alt => 'Mixi'), 'http://mixi.jp/show_friend.pl?id=' + service.key
     when 'github'
-      link_to image_tag("Github.png"), 'http://github.com/' + service.key
+      link_to image_tag("Github.png", :alt => 'Github'), 'http://github.com/' + service.key
     when 'facebook'
       if service.key =~ /^\s*\d+$/
-        link_to image_tag("Facebook.png"), 'http://www.facebook.com/profile.php?id=' + service.key
+        link_to image_tag("Facebook.png", :alt => 'Facebook'), 'http://www.facebook.com/profile.php?id=' + service.key
       else
-        link_to image_tag("Facebook.png"), 'http://www.facebook.com/' + service.key
+        link_to image_tag("Facebook.png", :alt => 'Facebook'), 'http://www.facebook.com/' + service.key
       end
     when 'iddy'
-      link_to image_tag("Iddy.png"), 'http://iddy.jp/profile/' + service.key
+      link_to image_tag("Iddy.png", :alt => 'Iddy'), 'http://iddy.jp/profile/' + service.key
     end
   end
   
